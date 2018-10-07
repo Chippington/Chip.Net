30265721b33cb5a0df759a68302f19d3d13aa5fa
Chippington <scythix@hotmail.com>
2018-10-07 14:51:42 -0700

    Initial commit

---
+++ Chip.Net/Chip.Net.Testbed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chip.Net.Testbed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
