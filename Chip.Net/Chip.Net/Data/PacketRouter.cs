15cbd0e36e7ee3eda6f7658272eb42457b668aaf
Chippington <scythix@hotmail.com>
2018-10-10 19:31:57 -0700

    Basic packet router implementation

---
+++ Chip.Net/Chip.Net/Data/PacketRouter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chip.Net.Data {
+	internal class Callback {
+		private Type type;
+		private Action<Packet> callback;
+		public Callback(Type type, Action<Packet> callback) {
+			this.callback = callback;
+			this.type = type;
+		}
+
+		public virtual void Invoke(Packet packet) {
+			callback.Invoke(packet);
+		}
+	}
+
+	internal class Callback<T> : Callback where T : Packet {
+		private Action<T> callback;
+		public Callback(Action<T> callback) : base(typeof(T), i => callback((T)i)) {
+			this.callback = callback;
+		}
+
+		public override void Invoke(Packet packet) {
+			base.Invoke(packet);
+		}
+	}
+
+
+	public class PacketRouter {
+		private Dictionary<Type, List<Callback>> clientRouteMap;
+		private Dictionary<Type, List<Callback>> serverRouteMap;
+
+		public PacketRouter() {
+			clientRouteMap = new Dictionary<Type, List<Callback>>();
+			serverRouteMap = new Dictionary<Type, List<Callback>>();
+		}
+
+		void Route<T>(Action<T> callback) where T : Packet {
+			GetList(clientRouteMap, typeof(T)).Add(new Callback<T>(callback));
+			GetList(serverRouteMap, typeof(T)).Add(new Callback<T>(callback));
+		}
+
+		void RouteClient<T>(Action<T> callback) where T : Packet {
+			GetList(clientRouteMap, typeof(T)).Add(new Callback<T>(callback));
+		}
+
+		void RouteServer<T>(Action<T> callback) where T : Packet {
+			GetList(serverRouteMap, typeof(T)).Add(new Callback<T>(callback));
+		}
+
+		void OnReceive(Packet packet) {
+			var cl = GetList(clientRouteMap, packet.GetType(), false);
+			var sv = GetList(serverRouteMap, packet.GetType(), false);
+
+			if (cl != null) {
+				for (int i = 0; i < cl.Count; i++) {
+					cl[i].Invoke(packet);
+				}
+			}
+
+			if (sv != null) {
+				for (int i = 0; i < sv.Count; i++) {
+					sv[i].Invoke(packet);
+				}
+			}
+		}
+
+		private List<Callback> GetList(Dictionary<Type, List<Callback>> map, Type type, bool create = true) {
+			if (map.ContainsKey(type) == false) {
+				if (create == false)
+					return null;
+
+				map.Add(type, new List<Callback>());
+			}
+
+			return map[type];
+		}
+	}
+}
