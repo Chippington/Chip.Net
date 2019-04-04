ec49301c160e6584dc7e595a3c41aace05d0a9f9
Chippington <scythix@hotmail.com>
2019-04-03 21:09:23 -0700

    Changed router/shard/user test classes to extend base test class

--- Chip.Net/Chip.Net.UnitTests/Controllers/Distributed/RouterShardTests.cs
+++ Chip.Net/Chip.Net.UnitTests/Controllers/Distributed/RouterShardTests.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Chip.Net.Controllers.Distributed;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Chip.Net.UnitTests.Controllers.Distributed
 {
-    class RouterShardTests
-    {
+	[TestClass]
+    public class RouterShardTests : BaseControllerTests<RouterServer, ShardClient> {
     }
 }
--- Chip.Net/Chip.Net.UnitTests/Controllers/Distributed/RouterUserTests.cs
+++ Chip.Net/Chip.Net.UnitTests/Controllers/Distributed/RouterUserTests.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Chip.Net.Controllers.Distributed;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Chip.Net.UnitTests.Controllers.Distributed
 {
-    class RouterUserTests
-    {
-    }
+	[TestClass]
+    public class RouterUserTests : BaseControllerTests<RouterServer, UserClient> {
+	}
 }
