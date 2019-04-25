37517dbf3db3272f00345968ee022a370032fd37
Chippington <scythix@hotmail.com>
2019-04-25 08:26:26 -0700

    Added bounceback tests

--- Chip.Net/Chip.Net.UnitTests/Controllers/Distributed/ShardUserTests.cs
+++ Chip.Net/Chip.Net.UnitTests/Controllers/Distributed/ShardUserTests.cs
@@ -114,7 +114,7 @@ namespace Chip.Net.UnitTests.Controllers.Distributed
 		}
 
 		[TestMethod]
-		public void Shard_SendToOne_UserReceivedMessage() {
+		public void Shard_SendToOneUser_UserReceivedMessage() {
 			var shard = Shards.First();
 			var user = Users.First();
 			var pck = new TestPacket();
@@ -125,19 +125,19 @@ namespace Chip.Net.UnitTests.Controllers.Distributed
 		}
 
 		[TestMethod]
-		public void Shard_SendToMany_UsersReceivedMessages() {
+		public void Shard_SendToManyUsers_UsersReceivedMessages() {
 			var shard = Shards.First();
 			var userModels = Users.Select(i => i.User.Model);
 			var pck = new TestPacket();
 
 			shard.Passthrough.Send(pck, userModels);
-			Assert.IsTrue(UsersReceived.Count == Shards.Count);
+			Assert.IsTrue(UsersReceived.Count == Users.Count);
 			foreach (var user in Users)
 				Assert.IsTrue(UsersReceived.Contains(user));
 		}
 
 		[TestMethod]
-		public void User_SendToOne_ShardReceivedMessage() {
+		public void User_SendToOneShard_ShardReceivedMessage() {
 			var shard = Shards.First();
 			var user = Users.First();
 			var pck = new TestPacket();
@@ -148,7 +148,7 @@ namespace Chip.Net.UnitTests.Controllers.Distributed
 		}
 
 		[TestMethod]
-		public void User_SendToMany_ShardsReceivedMessages() {
+		public void User_SendToManyShards_ShardsReceivedMessages() {
 			var user = Users.First();
 			var shardModels = Shards.Select(i => i.Shard.Model);
 			var pck = new TestPacket();
@@ -157,6 +157,52 @@ namespace Chip.Net.UnitTests.Controllers.Distributed
 			Assert.IsTrue(ShardsReceived.Count == Shards.Count);
 			foreach (var shard in Shards)
 				Assert.IsTrue(ShardsReceived.Contains(shard));
+		}
+
+		[TestMethod]
+		public void Shard_SendToOneShard_UserReceivedMessage() {
+			var shard = Shards.First();
+			var recv = Shards.Skip(1).First();
+			var pck = new TestPacket();
+
+			shard.Passthrough.Send(pck, recv.Shard.Model);
+			Assert.IsTrue(ShardsReceived.Contains(recv));
+			Assert.IsTrue(ShardsReceived.Count == 1);
+		}
+
+		[TestMethod]
+		public void Shard_SendToManyShards_UsersReceivedMessages() {
+			var shard = Shards.First();
+			var shardModels = Shards.Select(i => i.Shard.Model);
+			var pck = new TestPacket();
+
+			shard.Passthrough.Send(pck, shardModels);
+			Assert.IsTrue(ShardsReceived.Count == Shards.Count);
+			foreach (var sh in Shards)
+				Assert.IsTrue(ShardsReceived.Contains(sh));
+		}
+
+		[TestMethod]
+		public void User_SendToOneUser_ShardReceivedMessage() {
+			var user = Users.First();
+			var recv = Users.Skip(1).First();
+			var pck = new TestPacket();
+
+			user.Passthrough.Send(pck, recv.User.Model);
+			Assert.IsTrue(UsersReceived.Contains(recv));
+			Assert.IsTrue(UsersReceived.Count == 1);
+		}
+
+		[TestMethod]
+		public void User_SendToManyUsers_ShardsReceivedMessages() {
+			var user = Users.First();
+			var userModels = Users.Select(i => i.User.Model);
+			var pck = new TestPacket();
+
+			user.Passthrough.Send(pck, userModels);
+			Assert.IsTrue(UsersReceived.Count == Users.Count);
+			foreach (var sh in Users)
+				Assert.IsTrue(UsersReceived.Contains(sh));
 		}
 	}
 }
