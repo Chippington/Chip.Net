447a3e4409b89787f606e8ae9769367fbfd7a632
Chippington <scythix@hotmail.com>
2019-04-28 10:34:13 -0700

    Created net channels in RegionService

--- Chip.Net/Chip.Net/Controllers/Distributed/Services/Regions/RegionService.cs
+++ Chip.Net/Chip.Net/Controllers/Distributed/Services/Regions/RegionService.cs
@@ -19,11 +19,31 @@ namespace Chip.Net.Controllers.Distributed.Services.Regions
 
 		public IRegionResolver<TRegion, TFocus> Resolver { get; private set; }
 
+		public ShardChannel<FocusPacket> ShardAddFocus { get; private set; }
+		public ShardChannel<FocusPacket> ShardRemoveFocus { get; private set; }
+		public ShardChannel<FocusPacket> ShardUpdateFocus { get; private set; }
+
+		public UserChannel<FocusPacket> UserAddFocus { get; private set; }
+		public UserChannel<FocusPacket> UserRemoveFocus { get; private set; }
+		public UserChannel<FocusPacket> UserUpdateFocus { get; private set; }
+
 		public ModelTrackerCollection<TFocus> FocusModels;
 
 		public RegionService(IRegionResolver<TRegion, TFocus> resolver) {
 			this.FocusModels = new ModelTrackerCollection<TFocus>();
 			this.Resolver = resolver;
+		}
+
+		protected override void InitializeDistributedService() {
+			base.InitializeDistributedService();
+
+			this.ShardAddFocus = CreateShardChannel<FocusPacket>("add");
+			this.ShardRemoveFocus = CreateShardChannel<FocusPacket>("remove");
+			this.ShardUpdateFocus = CreateShardChannel<FocusPacket>("update");
+
+			this.UserAddFocus = CreateUserChannel<FocusPacket>("add");
+			this.UserRemoveFocus = CreateUserChannel<FocusPacket>("remove");
+			this.UserUpdateFocus = CreateUserChannel<FocusPacket>("update");
 		}
 	}
 }
--- Chip.Net/Chip.Net/Controllers/Distributed/Services/Regions/RegionServicePackets.cs
+++ Chip.Net/Chip.Net/Controllers/Distributed/Services/Regions/RegionServicePackets.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Chip.Net.Controllers.Distributed.Models;
+using Chip.Net.Data;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Chip.Net.Controllers.Distributed.Services.Regions
 {
-    class RegionServicePackets
-    {
-    }
+    public partial class RegionService<TRegion, TFocus> where TRegion : IRegionModel where TFocus : IDistributedModel {
+		public class FocusPacket : Packet {
+			public TFocus Focus { get; set; }
+
+			public override void WriteTo(DataBuffer buffer) {
+				base.WriteTo(buffer);
+				Focus.WriteTo(buffer);
+			}
+
+			public override void ReadFrom(DataBuffer buffer) {
+				base.ReadFrom(buffer);
+				this.Focus = Activator.CreateInstance<TFocus>();
+				this.Focus.ReadFrom(buffer);
+			}
+		}
+	}
 }
