c87d5bdbfb4b4902a5008d5572aabace550f1b98
Chippington <scythix@hotmail.com>
2019-04-28 22:35:03 -0700

    Changed shard/user parts of RegionService to compile

--- Chip.Net/Chip.Net/Controllers/Distributed/Services/Regions/RegionService.Shard.cs
+++ Chip.Net/Chip.Net/Controllers/Distributed/Services/Regions/RegionService.Shard.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Chip.Net.Controllers.Distributed.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Chip.Net.Controllers.Distributed.Services.Regions
 {
-    class RegionService
-    {
-    }
+	public partial class RegionService<TRegion, TFocus> : DistributedService
+		where TRegion : IRegionModel
+		where TFocus : IDistributedModel {
+
+	}
 }
